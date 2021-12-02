5aa426dbb1339611b9e33f659de7381298750677
matheusgomes062 <matheusgomes062@gmail.com>
2021-12-02 01:57:27 -0300

    new ci

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -1,11 +1,13 @@
+/* eslint-disable no-console */
 const execa = require('execa');
 const fs = require('fs');
-
 (async () => {
   try {
     await execa('git', ['checkout', '--orphan', 'gh-pages']);
-    console.log('Building...');
+    // eslint-disable-next-line no-console
+    console.log('Building started...');
     await execa('npm', ['run', 'build']);
+    // await execa("yarn", ["build"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync('dist') ? 'dist' : 'build';
     await execa('git', ['--work-tree', folderName, 'add', '--all']);
@@ -15,8 +17,9 @@ const fs = require('fs');
     await execa('rm', ['-r', folderName]);
     await execa('git', ['checkout', '-f', 'main']);
     await execa('git', ['branch', '-D', 'gh-pages']);
-    console.log('Successfully deployed');
+    console.log('Successfully deployed, check your settings');
   } catch (e) {
+    // eslint-disable-next-line no-console
     console.log(e.message);
     process.exit(1);
   }
